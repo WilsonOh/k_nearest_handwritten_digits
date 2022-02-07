15bf1e8e4a1ee2a650759e01a17084605ffeeadd
WilsonOh <oh.wilson123@gmail.com>
2022-02-07 19:03:54 +0800

    small update

--- k_nearest.py
+++ k_nearest.py
@@ -1,5 +1,5 @@
+from classes import Test, Neighbour
 import digits
-from classes import Neighbour, Test
 
 K = 5
 
@@ -8,6 +8,7 @@ training_samples = digits.initialise_training_samples(Neighbour, train_file)
 
 
 def init(test_digit):
+    """Creates a list of test samples and training samples by reading from stdin"""
     test_sample = digits.initialise_testing_sample(Test, test_digit)
     print("Analyzing digit...")
-    print(digits.find_most_common(training_samples, test_sample, K), end='')
+    print(f"My guess is: {digits.find_most_common(training_samples, test_sample, K)}", end='')
